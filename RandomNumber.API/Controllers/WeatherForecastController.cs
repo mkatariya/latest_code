508817f894e7cc44731666ef017ba4056f1a524d
mohit katariya <katariya.mohit@gmail.com>
2021-03-13 16:41:41 +0530

    added VS Folder

---
+++ RandomNumber.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RandomNumber.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //[Route("api/getSeries/{n}")]
+        public async Task<IActionResult> Get(int n=15)
+        {
+            //Need to create series 1,1,1,3,5,9,17,31,57,105...... Sum of last 3 digits...initial 3 are fixed i.e. 1,1,1
+            //need to pass Nth Number till which need to generate series
+            //List<int> ar = new List<int>(); 
+            
+            ArrayList arr = new ArrayList();
+            arr.Add(1);
+            arr.Add(1);
+            arr.Add(1);
+            if (n > 2)
+            {
+                for (int i = 3; i < n; i++)
+                {
+                    arr.Add(Convert.ToInt32(arr[i - 3]) + Convert.ToInt32(arr[i - 2]) + Convert.ToInt32(arr[i - 1]));
+                }
+            }
+
+            return Ok(  arr);
+        }
+    }
+}
---
+++ RandomNumber.MVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RandomNumber.MVC.Models;
+
+namespace RandomNumber.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44365/weatherforecast"); //Need to add API addreess 
+                var result = client.GetAsync("weatherforecast").Result;
+                //result.Wait();
+
+                if (result.IsSuccessStatusCode)
+                {
+                   var output =  result.Content.ReadAsStringAsync().Result;
+                    var series = JsonConvert.DeserializeObject<List<int>>(output);
+                }
+
+
+            }
+
+                return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
